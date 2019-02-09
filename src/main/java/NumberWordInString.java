86f7faa5ad9f9b3140912cd315f2da3031b7f19a
Vitaliy <vetal.t28@gmail.com>
2019-02-10 00:45:06 +0200

    Определить к-сто слов в строке

---
+++ src/main/java/NumberWordInString.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class NumberWordInString {
+
+    /*** определить количество слов в строке в Java */
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        //Вводим предложение в консоль
+        System.out.println("Введите слова одной строкой через пробел");
+        String input = sc.nextLine();
+        //Начальное количество слов равно 0
+        int count = 0;
+        //Если ввели хотя бы одно слово, тогда считать, иначе конец программы
+        if (input.length() != 0) {
+            count++;
+            //Проверяем каждый символ, не пробел ли это
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) == ' ') {
+                    //Если пробел - увеличиваем количество слов на 1
+                    count++;
+                }
+            }
+        }
+        System.out.println("Вы ввели " + count + "слов");
+    }
+}
